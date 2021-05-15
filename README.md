c27c6cef192e4177c83e46665d437521d4e42add
riasat <riasat.jamil@outlook.com>
2021-05-16 02:43:39 +0600

    more testing and results.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 A CSS Grid Responsive Table Experiment
 
 Just a personal experiment with CSS Grid to achieve something that, in theory, should work.
+
+# Results
+
+I remember it would have been possible to use one grid for this thing, but the main issue seems to be they dropping `subgrid` from the spec and moving it to level 2.
+So we can do this in a few ways:
+
+- Use a full `<table><tr><td>` for large views
+- Set `table` to `display: block;`, `tr` to `display: grid;`. We can turn `tr` to a card for smaller view.
+- Use one grid for large views but group each row using `div` (like `tr`) and set them to `display: contents;`. For smaller view, set those grouping `div`s to `display: grid;`.
+- Use named zones for the whole grid. Something like: `grid-template-areas: repeat(8, "Name Cores Threads Clockspeed Turbospeed TDP Price");`
+  but it doesn't support repeat, so we can just loop it in js and add row numbers to the zone names. This will work really nicely, but a bit of hassle to set up. And ofc, cant be done without js.
+- The best way. Use `subgrid`. But as of now, only latest FF has implemented it.
+
--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 import {cpu9 as cpu } from "./data";
 
 export function printCpu() {
+  // noinspection SpellCheckingInspection
   cpu.forEach(({Clockspeed, Cores, Name, Price, Release, TDP, Threads, Turbospeed}) => {
-    simpleCell(Name);
-    simpleCell(Cores);
-    simpleCell(Threads);
-    simpleCell(Clockspeed);
-    simpleCell(Turbospeed);
-    simpleCell(TDP);
-    simpleCell(Release);
-    simpleCell(Price);
+    const content = document.createElement('div');
+    content.className = 'rowGroup';
+    content.id = 'rowGroup' + Math.floor(Math.random() * 10000);
+    document.getElementById('grid').appendChild(content);
+    simpleCell(Name, content.id);
+    simpleCell(Cores, content.id);
+    simpleCell(Threads, content.id);
+    simpleCell(Clockspeed, content.id);
+    simpleCell(Turbospeed, content.id);
+    simpleCell(TDP, content.id);
+    simpleCell(Release, content.id);
+    simpleCell(Price, content.id);
   });
 }
 
-export function simpleCell(value) {
+export function simpleCell(value, id) {
   const cell = document.createElement('div');
   cell.innerText = value;
-  document.getElementById("grid").appendChild(cell);
+  document.getElementById(id).appendChild(cell);
 }
 
 printCpu();
